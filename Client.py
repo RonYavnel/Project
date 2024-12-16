2c43e0a279eaa3e5ca7da54ff6b19139c2e8cc0a
RonYavnel <ron.yavnel@gmail.com>
2024-12-16 15:43:04 +0200

    The client is commented. TODO: continue documentation.

--- Client.py
+++ Client.py
@@ -12,30 +12,39 @@ client_socket = socket.socket()
 client_socket.connect((HOST, PORT))
 
 # Fill in personal details - username and password
-name = input("Enter your name: ")
-client_socket.send(name.encode())
+# Send them to the server
+username = input("Enter your username: ")
+client_socket.send(username.encode())
 
 password = input("Enter your password: ")
 client_socket.send(password.encode())
 
+# Recieve the most updated
 share_price = int(client_socket.recv(1024).decode())
 print(f"Current share price: {share_price}")
 
+# Check if client with this characteristics - the server sends the answer:
+# 1 if registered and 0 if not
 is_registered = client_socket.recv(1024).decode()
+
+# If not registered, try and recieve the balance of the new client until it is valid
 if (is_registered == '0'):
     while True:
         try:
             balance = int(input("Enter your balance: "))
+            # If valid - break from the loop
             break
         except ValueError:
             print("Invalid balance entered. Please enter a digital value.")
         
     client_socket.send(str(balance).encode())
     
+# If the client exists, present the balance to him and continue
 else:
     current_balance = client_socket.recv(1024).decode()
     print(f"Your current balance: {current_balance}")    
     
+# Listen to client's orders until he sends an empty message
 while True:
     order = input("enter your order (side$amount): ")
     client_socket.send(order.encode())
@@ -43,9 +52,11 @@ while True:
         client_socket.close()
         break
     
+    # Recieve the appropriate response from the server about the order
     response = client_socket.recv(1024).decode()
     print(response)
     
+    # Recieve the updated share price from the server
     share_price = int(client_socket.recv(1024).decode())
     print(f"New share price: {share_price}")    
 
